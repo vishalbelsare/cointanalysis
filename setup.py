3d5fb13cb774a7b084b1a6793744b8eeb6d12eb5
Shota Imaki <shota.imaki@icloud.com>
2020-01-03 18:06:24 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     maintainer='Shota Imaki',
     maintainer_email='shota.imaki@icloud.com',
     url='https://github.com/simaki/cointanalysis',
-    packages=find_packages(include=["coint", "coint.*"]),
+    packages=find_packages(include=["cointanalysis", "cointanalysis.*"]),
     license=license,
     classifiers=[
         'Intended Audience :: Financial and Insurance Industry',
